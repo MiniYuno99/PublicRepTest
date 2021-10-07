49ef9f9227604d783e0d15fdd013656b678e1bce
nanacchi99 <paumartgarcia@gmail.com>
2021-10-07 13:54:55 +0200

    line

--- PublicTest/src/main/java/main/Test.java
+++ PublicTest/src/main/java/main/Test.java
@@ -1,5 +1,10 @@
 package main;
 
-public class Test {
+import java.util.Scanner;
 
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Test");
+    }
 }
